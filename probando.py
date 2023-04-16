20ce845022913f2c955e96af44af8bd2f334f9c8
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-04-17 01:11:31 +0200

    Add files via upload

--- probando.py
+++ probando.py
@@ -1,3 +1,18 @@
 import streamlit as st
-a=st.text_input("esto que es")
-st.text(a)
+st.header("La vida en el mar")
+listilla=["sardina","mero","tiburón ballena","pulpo"]
+elec_animal=st.radio("Elige el animal que más te gusta:",listilla)
+if elec_animal==listilla[0]:
+    st.error("por el monte la sardina")
+elif elec_animal==listilla[1]:
+    st.warning("uno mero dos de febrero")
+elif elec_animal==listilla[2]:
+    st.succes("por fin llegamos a lo que interesa")
+elif elec_animal==listilla[4]:
+    st.text("elige tiburón ballena por favor")
+if(st.button("Click me for no reason")):
+    st.markdown(":red[lo estás haciendo muy bien]")
+elec_foto=st.selectbox("Fotos:",
+                     ['F1', 'F2', 'F3'])
+fotillo=Image.open(elec_foto)
+st.image(fotillo, width=200)
