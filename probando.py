3003da0a61b3e4fb605f642622706caeff451c58
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-04-17 01:29:53 +0200

    Add files via upload

--- probando.py
+++ probando.py
@@ -14,6 +14,8 @@ if(st.button("Click me for no reason")):
     st.markdown(":red[lo estás haciendo muy bien]")
 elec_foto=st.selectbox("Fotos:",
                      ['F1', 'F2', 'F3'])
+st.image(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.bekaretransfers.com%2Fblog%2Fwp-content%2Fuploads%2F2017%2F05%2Fwhaleshark.jpeg&f=1&nofb=1&ipt=968b7797e170594c622da1f04f2fca3cc7de1aea4d0b823b37a3836f67433291&ipo=images)
+st.image(F1)
 fotoguay=Image.open(F1.jpg)
 st.image(fotoguay,width=200)
 fotillo=Image.open(elec_foto)
